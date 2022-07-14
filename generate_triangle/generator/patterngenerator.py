29d926e3e7711414abe9a897a30983800b2b3a5f
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-07-14 15:41:48 +0530

    Final commit

--- generate_triangle/generator/patterngenerator.py
+++ generate_triangle/generator/patterngenerator.py
@@ -19,25 +19,24 @@ class PatternPrinter:
 
     def generate_pattern(self) -> None:
         if self.number_of_rows == 1:
-            self.final_pattern_list == self.generate_pattern_for_1_row()
+            self.final_pattern_list = self.generate_pattern_for_one_row()
         elif self.number_of_rows == 2:
-            self.final_pattern_list == self.generate_pattern_for_2_rows()
+            self.final_pattern_list = self.generate_pattern_for_2_row()
         else:
-            self.final_pattern_list == self.generate_pattern_for_2_rows()
+            self.final_pattern_list = self.generate_pattern_for_2_row()
             for index in range(3, self.number_of_rows+1):
                 calculated_row_list = self.generate_pattern_for_next_row(self.final_pattern_list[-1])
                 self.final_pattern_list.append(calculated_row_list)
-
+        
         self.print_output()
 
-    
-    def generate_pattern_for_1_row(self):
+    def generate_pattern_for_one_row(self):
         return [[1]]
 
-    def generate_pattern_for_2_rows(self):
+    def generate_pattern_for_2_row(self):
         return [[1], [1,1]]
-
-    def generate_pattern_for_next_row(self,previous_row_list:list) -> list:
+    
+    def generate_pattern_for_next_row(self, previous_row_list:list) -> list:
         final_output_list = []
         final_output_list.append(1)
         for i, item in enumerate(previous_row_list):
@@ -48,7 +47,9 @@ class PatternPrinter:
             finally:
                 final_output_list.append(sum_value)
         return final_output_list
-    
+
     def print_output(self):
         for item in self.final_pattern_list:
-            print(item)
+            for sub_item in item:
+                print(sub_item, end="  ")
+            print()
