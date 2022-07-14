e1a2c24a71bf7f8421a96674a9e37e3b62cea5b6
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-07-14 15:12:46 +0530

    Added major methods

--- generate_triangle/generator/patterngenerator.py
+++ generate_triangle/generator/patterngenerator.py
@@ -17,4 +17,33 @@ class PatternPrinter:
         else:
             return input_val
 
+    def generate_pattern(self) -> None:
+        if self.number_of_rows == 1:
+            self.final_pattern_list == self.generate_pattern_for_1_row()
+        elif self.number_of_rows == 2:
+            self.final_pattern_list == self.generate_pattern_for_2_rows()
+        else:
+            self.final_pattern_list == self.generate_pattern_for_2_rows()
+            for index in range(3,self.number_of_rows+1):
+                calculated_row_list = self.generate_pattern_for_next_row(self.final_pattern_list[-1])
+                self.final_pattern_list.append(calculated_row_list)
+        self.print_output()
+
     
+    def generate_pattern_for_1_row(self):
+        return [1]
+
+    def generate_pattern_for_2_rows(self):
+        return [[1], [1,1]]
+
+    def generate_pattern_for_next_row(self,previous_row_list:list) -> list:
+        final_output_list = []
+        final_output_list.append(1)
+        for i, item in enumerate(previous_row_list):
+            try:
+                sum_value = item + previous_row_list[i+1]
+            except IndexError:
+                sum_value = item + 0
+            finally:
+                final_output_list.append(sum_value)
+            return final_output_list
